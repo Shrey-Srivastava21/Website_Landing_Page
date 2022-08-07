6a63fdf6e6369e543db3b906c2d835cc07545ba2
Shrey-Srivastava21 <devshiva.1738@gmail.com>
2022-08-07 20:02:31 +0530

    Landing Page

---
+++ src/components/footer.jsx
@@ -0,0 +1,17 @@
+export const Footer = (props) => {
+  
+  return (
+    <div>
+      <div id='footer'>
+        <div className='container text-center'>
+          <p>
+            &copy; 2022 Shrey Srivastava React Land Page Template
+            {/* <a href='http://www.templatewire.com' rel='nofollow'>
+              TemplateWire
+            </a> */}
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
